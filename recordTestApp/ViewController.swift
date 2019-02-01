ac2ed6408b4044ec9bb03441e964dee8b2396e3c
amaocha <guiltaugusthird03@gmail.com>
2019-02-01 14:18:07 +0900

    Initial Commit

---
+++ recordTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  recordTestApp
+//
+//  Created by coco j on 2019/02/01.
+//  Copyright © 2019 coco j. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
